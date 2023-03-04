b2726f1aee79f8021d14730cfe6869e3192e8224
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2023-03-04 23:04:08 +0300

    add files

---
+++ db/migrate/20230304200322_create_meme.rb
@@ -0,0 +1,10 @@
+class CreateMemes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :memes do |t|
+      t.string :title
+      t.string :your_meme
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
